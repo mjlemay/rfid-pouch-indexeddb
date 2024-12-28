d9dbd5d39265b996a5e0de99b8affbe2f60b7b93
Michael LeMay <mjlemay@gmail.com>
2024-12-28 15:37:03 -0800

    updates export to render correctly

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  images: {
+      unoptimized: true,
+  },
+  distDir: 'out', 
+  trailingSlash: true,
+  assetPrefix: '.',
+  staticPageGenerationTimeout: 1000,
+  reactStrictMode: false
 };
 
 export default nextConfig;
